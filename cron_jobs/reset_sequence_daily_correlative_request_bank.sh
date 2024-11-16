6e58e2816e740ae98f7d5145633d922998ec2c54
nelmerayala <nelmerayala@gmail.com>
2024-11-16 10:01:31 -0400

    ajustes en folder cron_jobs 2

--- cron_jobs/reset_sequence_daily_correlative_request_bank.sh
+++ cron_jobs/reset_sequence_daily_correlative_request_bank.sh
@@ -3,8 +3,16 @@
 # Definir la variable de entorno para la contraseña
 export PGPASSWORD='administrator'
 
-# Ejecutar el comando de reinicio de secuencia
-psql -U postgres -d siartec -c "SELECT reset_sequence('daily_correlative_request_bank');"
+# Definir la ruta para el archivo de log, incluyendo la fecha
+LOG_PATH="/logs/reset_sequences/daily_correlative_request_bank/reset_sequence_$(date +\%Y-\%m-\%d).log"
+
+# Ejecutar el comando y redirigir la salida al archivo de log
+{
+  echo "Ejecutando reinicio de secuencia: $(date)"
+  # Ejecutar el comando de reinicio de secuencia
+  psql -U postgres -d siartec -c "SELECT reset_sequence('daily_correlative_request_bank');"
+  echo "Finalización del script: $(date)"
+} >> "$LOG_PATH" 2>&1
 
 # Desexportar la variable para mayor seguridad
 unset PGPASSWORD
