af51989435dc38ce836d615970e7e8e29ff36929
nelmerayala <nelmerayala@gmail.com>
2024-11-16 11:28:15 -0400

    ajustes en folder cron_jobs 3

---
+++ cron_jobs/run_calculation_factor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Define el URL del endpoint
+API_URL="http://101.1.1.97:8020/calculation-factor/synchronize"
+
+# Definir la ruta para el archivo de log, incluyendo la fecha
+LOG_PATH="/logs/endpoints/calculation_factor/create_calculation_factor_$(date +\%Y-\%m-\%d).log"
+
+# Ejecutar el comando y redirigir la salida al archivo de log
+{
+  echo "Ejecutando consumo de endpoint: $(date)"
+  
+  # Ejecutar el comando curl a endpoint /calculation-factor/synchronize
+  RESPONSE=$(curl --silent --request GET $API_URL)
+  
+  # Muestra la respuesta
+  echo "Respuesta del servidor:"
+  echo "$RESPONSE"
+
+  echo "Finalización del script: $(date)"
+} >> "$LOG_PATH" 2>&1
+
+# Desexportar la variable para mayor seguridad
+unset API_URL
